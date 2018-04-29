547b65d0d04be262cf88d6b53f33925a322d8045
ByeDream <helloworld_se7en@yeah.net>
2018-04-30 02:38:24 +0800

    [*] Done DX12 viewer

--- RayTracer/RayTracer/D3D12Viewer.h
+++ RayTracer/RayTracer/D3D12Viewer.h
@@ -1,32 +1,36 @@
 #pragma once
 
+class OutputImage;
+
 class D3D12Viewer
 {
 public:
-	D3D12Viewer(HWND hwnd, unsigned width, unsigned height);
+	D3D12Viewer(HWND hwnd, UINT32 width, UINT32 height, const OutputImage *outputImage);
 	~D3D12Viewer();
 
 	void										OnInit();
 	void										OnUpdate();
 	void										OnRender();
 	void										OnDestroy();
-	void										OnKeyDown(unsigned char keyCode);
-	void										OnKeyUp(unsigned char keyCode);
+	void										OnKeyDown(UINT8 keyCode);
+	void										OnKeyUp(UINT8 keyCode);
 
 private:
 	void										LoadPipeline();
 	void										LoadAssets();
 	void										PopulateCommandList();
-	void										WaitForPreviousFrame();
+	void										WaitForGpu();
+	void										MoveToNextFrame();
 
 private:
-	static const unsigned						FrameCount = 3;
+	static const UINT32							FrameCount = 3;
 
 	HWND										m_hwnd;
-	unsigned									m_frameIndex{ 0 };
-	unsigned									m_width{ 1028 };
-	unsigned									m_height{ 720 };
+	UINT32										m_frameIndex{ 0 };
+	UINT32										m_width{ 1028 };
+	UINT32										m_height{ 720 };
 	float										m_aspectRatio;
+	const OutputImage *							m_image;
 
 	CD3DX12_VIEWPORT							m_viewport;
 	CD3DX12_RECT								m_scissorRect;
@@ -34,22 +38,26 @@ private:
 	ComPtr<ID3D12CommandQueue>					m_commandQueue;
 	ComPtr<IDXGISwapChain3>						m_swapChain;
 	ComPtr<ID3D12Resource>						m_renderTargets[FrameCount];
-	ComPtr<ID3D12CommandAllocator>				m_commandAllocator;
+	ComPtr<ID3D12CommandAllocator>				m_commandAllocators[FrameCount];
 	ComPtr<ID3D12RootSignature>					m_rootSignature;
 	ComPtr<ID3D12PipelineState>					m_pipelineState;
 	ComPtr<ID3D12GraphicsCommandList>			m_commandList;
 	ComPtr<ID3D12DescriptorHeap>				m_rtvHeap;
-	unsigned									m_rtvDescriptorSize{ 0 };
+	UINT32										m_rtvDescriptorSize{ 0 };
+	ComPtr<ID3D12DescriptorHeap>				m_srvHeap;
+	ComPtr<ID3D12Resource>						m_texture;
+	ComPtr<ID3D12Resource>						m_textureUploadHeap;
+	UINT64										m_uploadBufferSize{ 0 };
 
 	HANDLE										m_fenceEvent;
 	ComPtr<ID3D12Fence>							m_fence;
-	UINT64										m_fenceValue;
+	UINT64										m_fenceValues[FrameCount];
 
 	// vertexs.. TODO remove
 	struct Vertex
 	{
 		XMFLOAT3 position;
-		XMFLOAT4 color;
+		XMFLOAT2 uv;
 	};
 	D3D12_VERTEX_BUFFER_VIEW					m_vertexBufferView;
 	ComPtr<ID3D12Resource>						m_vertexBuffer;
